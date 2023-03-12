73640ccb76753726c4904192fe75714f4cde44ca
Tatiana Rubtsova <rubtsova-tanyal@mail.ru>
2023-03-12 21:35:27 +0500

    jsx-js

---
+++ src/baseUrl.js
@@ -0,0 +1,6 @@
+export const baseUrl = 'http://192.168.1.155:3000'//home
+//export const baseUrl = 'http://10.3.7.14:3000' //college
+//export const baseUrl = 'http://10.4.3.41:3000'
+//export const baseUrl = 'http://172.20.10.12:3000'
+//export const baseUrl = 'http://10.3.8.136:3000'
+
--- src/navigation/screens/wallet.js
+++ src/navigation/screens/wallet.js
@@ -5,9 +5,24 @@ import CustomButton from '../../components/CustomButton';
 import axios from 'axios';
 import SimpleInput from "../../components/SimpleInput";
 import {baseUrl} from "../../baseUrl";
+import { SelectList } from 'react-native-dropdown-select-list'
+
+
 
 
 export default function WalletHomeScreen({ navigation }) {
+  const [data,setData] = React.useState([]);
+
+  React.useEffect(() => 
+  axios({
+    method: 'get',
+    url: `${baseUrl}/users`,
+  }).then(res => {
+    setData(res.data.map((item) => {
+      return {key: item.username, value: `${item.name} (${item.username})`}
+    }))
+  })
+,[])
   const [user, setUser] = useState(null)
     const [coinsAmount, onChangeAmount] = React.useState('');
     const [receiverUsername, onChangeReceiver] = React.useState('');
@@ -29,6 +44,7 @@ export default function WalletHomeScreen({ navigation }) {
     }
 
     const sendTransaction = () => {
+      console.log(            {coinsAmount ,receiverUsername, reason}        )
         axios.post(`${baseUrl}/transaction`,
             {coinsAmount ,receiverUsername, reason},
             {headers:{'Content-Type': 'application/json'}}
@@ -43,8 +59,14 @@ export default function WalletHomeScreen({ navigation }) {
       <Text style={{ fontSize: 26 }}> Доступно монет: {user?.coinsToSend}
       </Text>
       <SimpleInput value={coinsAmount} setValue={onChangeAmount} placeholder={"Введите сумму для перевода"} />
-      <SimpleInput value={receiverUsername} setValue={onChangeReceiver} placeholder={"Введите айди получателя"} />
-      <SimpleInput value={reason} setValue={onChangeReason} placeholder={"Введите причину перевода"} />
+      <SelectList style={{}}
+        setSelected={(val) => onChangeReceiver(val)} 
+        data={data} 
+        save="key"
+        boxStyles={{width:330}}
+        placeholder={"Введите айди или имя получателя"}
+    />
+    <SimpleInput value={reason} setValue={onChangeReason} placeholder={"Введите причину перевода"} />
       <CustomButton title="Отправить" onPress={sendTransaction} />
 
       <Ionicons
